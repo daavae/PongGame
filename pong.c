7ce7907268b46e094b1fddd3089f9aa7cb07f7b3
Aarya <aarya.dave123@gmail.com>
2025-08-25 19:19:56 +1000

    changed some colours and added better naming conventions plus added a seperator

--- pong.c
+++ pong.c
@@ -1,4 +1,3 @@
-
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_pixels.h>
@@ -8,18 +7,21 @@
 #include <SDL3/SDL_video.h>
 #include <stdio.h>
 
+#define HEIGHT 800
+#define WIDTH 600
+#define RECT_HEIGHT 100
+#define RECT_WIDTH 15
+
 int main(int argc, char *argv[]) {
   SDL_Init(SDL_INIT_VIDEO);
   bool quit = false;
   SDL_Window *window =
-      SDL_CreateWindow("Pong Game", 800, 600, SDL_WINDOW_RESIZABLE);
+      SDL_CreateWindow("Pong Game", HEIGHT, WIDTH, SDL_WINDOW_RESIZABLE);
   SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
-  SDL_FRect rectangle;
-  rectangle.y = 100;
-  rectangle.x = 100;
-  rectangle.w = 100;
-  rectangle.h = 100;
+  SDL_FRect ply1 = (SDL_FRect){100, 100, RECT_WIDTH, RECT_HEIGHT};
+  SDL_FRect ply2 = (SDL_FRect){700, 100, RECT_WIDTH, RECT_HEIGHT};
+  SDL_FRect seperator = (SDL_FRect){400, 0, 10, HEIGHT};
 
   while (!quit) {
     SDL_Event ev;
@@ -30,12 +32,18 @@ int main(int argc, char *argv[]) {
         break;
       }
     }
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    if (SDL_RenderFillRect(renderer, &rectangle)) {
-      printf("showing rectangle\n");
+    if (SDL_RenderFillRect(renderer, &ply1)) {
+      printf("showing rectangle 1\n");
+    }
+    if (SDL_RenderFillRect(renderer, &ply2)) {
+      printf("showing rectangle 2\n");
+    }
+    if (SDL_RenderFillRect(renderer, &seperator)) {
+      printf("showing seperator\n");
     }
 
     SDL_RenderPresent(renderer);
